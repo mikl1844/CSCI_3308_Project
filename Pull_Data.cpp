1d8edfd7e7e568289667dbc6d3ebc7403b33f64e
user <ryta9312@colorado.edu>
2015-11-09 23:04:29 -0700

    Added push ans pull data from the database functions.

---
+++ Pull_Data.cpp
@@ -0,0 +1,46 @@
+#include <my_global.h>
+#include <mysql.h>
+
+
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);
+}
+
+int Pull_Data(int id, char[16] field)
+{
+  char query[64];
+
+  MYSQL *con = mysql_init(NULL);
+
+  if(con == NULL)
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    return -1;
+  }
+  
+  if (mysql_real_connect(con,"localhost","root","","Project",0,NULL,0)==NULL)
+  {
+    finish_with_error(con);
+  }
+
+  sprintf(query, "SELECT %s FROM House_DB WHERE ID=%d" ,field, id);
+  if (mysql_query(con, query))
+  {
+    finish_with_error(con);
+  }
+
+  mysql_close(con);
+  return 0;
+}
+
+int main()
+{
+  int id = 0;
+  char column[16] = "Temperature";
+  result = Pull_Data(id, column);
+  printf("Temperature: %f", result);
+  exit(0);
+}
---
+++ Push_Data.cpp
@@ -0,0 +1,52 @@
+#include <my_global.h>
+#include <mysql.h>
+
+
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);
+}
+
+int Push_Data(int id, float data)
+{
+  char query[64];
+
+  MYSQL *con = mysql_init(NULL);
+
+  if(con == NULL)
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    return -1;
+  }
+  
+  if (mysql_real_connect(con,"localhost","root","","Project",0,NULL,0)==NULL)
+  {
+    finish_with_error(con);
+  }
+
+  sprintf(query, "UPDATE House_DB SET Temperature=%f WHERE ID=%d" ,data, id);
+  if (mysql_query(con, query))
+  {
+    finish_with_error(con);
+  }
+
+  mysql_close(con);
+  return 0;
+}
+
+int main()
+{
+  int id = 2;
+  float data = 73.37;
+  status = Push_Data(id, data);
+  if(status == 0)
+  {
+    exit(0);
+  }
+  else
+  {
+    exit(1);
+  }
+}
