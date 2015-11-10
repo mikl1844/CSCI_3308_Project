9fd126aa32ef194c2f9ce785cfa0ef4a198ebb5e
mikl1844 <mikl1844@gmail.com>
2015-11-09 17:41:50 -0700

    Updated Push_Lock function that should be ready for final use

--- Push_Lock.cpp
+++ Push_Lock.cpp
@@ -1,7 +1,6 @@
 #include <my_global.h>
 #include <mysql.h>
-#include <stdio.h>
-#include <string.h>
+
 
 void finish_with_error(MYSQL *con)
 {
@@ -10,10 +9,14 @@ void finish_with_error(MYSQL *con)
   exit(1);
 }
 
-int main(int argc, char **argv)
+int main()//want to make it look like Lock(int id, int status)
 {
   MYSQL *con = mysql_init(NULL);
   
+  char House_DB[50];
+  char House_Data[50];
+  int id = 1;
+  int status = 1;
   if(con == NULL)
   {
     fprintf(stderr, "%s\n", mysql_error(con));
@@ -24,13 +27,32 @@ int main(int argc, char **argv)
   {
     finish_with_error(con);
   }
-  if (mysql_query(con,"UPDATE House_DB SET SmartLock='close' WHERE ID=3"))//House_DB Query
+  if (status == 0)
   {
-    finish_with_error(con);
+    sprintf(House_DB, "UPDATE House_DB SET SmartLock='close' WHERE ID=%d",id);
+    if (mysql_query(con,House_DB))//House_DB Query
+    {
+      finish_with_error(con);
+    }
+    sprintf(House_Data, "UPDATE House_Data SET Lock_Status=%d",status);
+    if (mysql_query(con, House_Data))//House_Data Query
+    {
+      finish_with_error(con);
+    }
   }
-  if (mysql_query(con, "UPDATE House_Data SET Lock_Status=0"))//House_Data Query
+  else
   {
-    finish_with_error(con);
+    status = 1;
+    sprintf(House_DB, "UPDATE House_DB SET SmartLock='open' WHERE ID=%d",id);
+    if (mysql_query(con,House_DB))//House_DB Query
+    {
+      finish_with_error(con);
+    }
+    sprintf(House_Data, "UPDATE House_Data SET Lock_Status=%d",status);
+    if (mysql_query(con, House_Data))//House_Data Query
+    {
+      finish_with_error(con);
+    }
   }
   mysql_close(con);
   exit(0);
